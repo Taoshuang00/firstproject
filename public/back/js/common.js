8ac38c86a67ea1a8fb1cd0566c1b18fd06630efb
TS <taoshuang@outlook.com>
2018-01-11 21:51:53 +0800

    页面公用部分登录验证完成

---
+++ public/back/js/common.js
@@ -0,0 +1,53 @@
+/**
+ * Created by ts on 2018/1/11 0011.
+ */
+;(function () {
+
+    NProgress.configure({showSpinner:false});
+    $(document).ajaxStart(function () {
+        NProgress.start();
+    });
+    $(document).ajaxStop(function () {
+        setTimeout(function () {
+            NProgress.done()
+        },1000)
+    })
+
+    if(location.href.indexOf('login.html') == -1){
+
+        $.ajax({
+            type:'get',
+            url:'/employee/checkRootLogin',
+            success:function (info) {
+                 if(info.error == 400){
+                    location.href = 'login.html';
+                }
+            }
+        })
+    }
+
+    $('.category').on('click',function () {
+
+        $('.list').slideToggle();
+    })
+    $('.icon-main').on('click',function () {
+        $('.lt_aside').toggle('.toggle')
+    })
+
+    $('.icon-out').on('click',function () {
+        $('#myModal').modal('show')
+    })
+
+    $('.btn-loginOut').on('click',function () {
+        $.ajax({
+            type:'get',
+            url:'/employee/employeeLogout',
+            success:function (info) {
+                if(info.success){
+                    console.log(info.success);
+                    //location.href = 'login.html';
+                }
+            }
+        })
+    })
+})();
