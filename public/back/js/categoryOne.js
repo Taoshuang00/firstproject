8dcc295cca4433b1de1f180e415942460397e957
TS <taoshuang@outlook.com>
2018-01-13 20:36:00 +0800

    一级分类完成

--- public/back/js/categoryOne.js
+++ public/back/js/categoryOne.js
@@ -8,14 +8,69 @@
     render();
     function render() {
         $.ajax({
-            type:'post',
+            type:'get',
             url:'/category/queryTopCategoryPaging',
             data:{
-                page:page
+                page:page,
+                pageSize:pageSize
             },
             success:function (info) {
+                console.log(info);
+                $('tbody').html(template('tpl-cate',info));
+
+                $('#paginator').bootstrapPaginator({
+                    bootstrapMajorVersion:3,
+                    currentPage:page,
+                    totalPages:Math.ceil(info.total/info.size),
+                    onPageClicked:function (a,b,c,p) {
+                        page = p ;
+                        render();
+                    }
+
+                })
+
 
             }
         })
     }
+
+    $('.btn-add ').on('click',function () {
+        $('#addModal').modal('show');
+    })
+
+    $('#form').bootstrapValidator({
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields:{
+            categoryName:{
+                validators:{
+                    notEmpty:{
+                        message:"请输入一级分类的名称"
+                    }
+                }
+            }
+        }
+    })
+
+    $("#form").on('success.form.bv',function (e) {
+        e.preventDefault();
+        $.ajax({
+            type:"post",
+            url:'/category/addTopCategory',
+            data:$('#form').serialize(),
+            success:function (info) {
+                console.log(info);
+                if(info.success){
+                    page= 1;
+                    render();
+                }
+            }
+        })
+        $('#addModal').modal('hide');
+    })
+
+    $("#form").data('bootstrapValidator').resetForm()
 })();
